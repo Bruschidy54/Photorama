1a62231d57a433679ce5c70088b77e1982678335
Dylan Bruschi <dylanbruschi54@gmail.com>
2017-03-05 20:24:43 -0600

    Finished photoinfoviewcontroller

---
+++ Photorama/PhotoInfoViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoInfoViewController.swift
+//  Photorama
+//
+//  Created by Dylan Bruschi on 3/5/17.
+//  Copyright © 2017 Dylan Bruschi. All rights reserved.
+//
+
+import UIKit
+
+class PhotoInfoViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    var photo: Photo! {
+        didSet {
+            navigationItem.title = photo.title
+        }
+    }
+    var store: PhotoStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        store.fetchImageForPhoto(photo: photo, completion: { (result) -> Void in
+            switch result {
+            case let .Success(image):
+                OperationQueue.main.addOperation {
+                    self.imageView.image = image
+                }
+            case let .Failure(error):
+                print("Error fetching image for photo: \(error)")
+            }
+            })
+    }
+}
